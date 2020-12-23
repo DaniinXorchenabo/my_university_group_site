be21ef0326f1c806819ccf381cf43fb86cc0c8f0
DaniinXorchenabo <rkbcu@mail.ru>
2020-12-23 18:15:03 +0300

    fixed bugs

--- app/web/main.py
+++ app/web/main.py
@@ -4,12 +4,13 @@
 
 from fastapi import FastAPI
 
-app = FastAPI()
-
 from app.web.routers.api_app import *
+
+
+app = FastAPI()
 
 app.include_router(api_app)
 
 if __name__ == "__main__":
     is_DB_created()
-    uvicorn.run("main_web:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
