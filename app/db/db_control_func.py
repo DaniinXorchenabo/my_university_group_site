38aee5231cc8c63b5010537b7d24d757d02764c3
DaniinXorchenabo <rkbcu@mail.ru>
2020-12-24 16:25:35 +0300

    Добалена функция коннекта с БД для версии Pony 0.73

--- app/db/db_control_func.py
+++ app/db/db_control_func.py
@@ -52,9 +52,9 @@ def make_migrate_file(db_l=db):
     sys.exit()
 
 
-def is_DB_created(db_path=DB_PATH, deep=0, db_l=db):
+def connect_with_db(db_path=DB_PATH, deep=0, db_l=db):
     """
-    Создает соединение с БД
+    Создает соединение с БД для Pony ORM версии 0.8
     :param db_path: путь к БД
     :param deep: глубина рекурсии
     :param db_l: объект БД
@@ -76,17 +76,11 @@ def is_DB_created(db_path=DB_PATH, deep=0, db_l=db):
                      create_db=True,
                      provider=cfg.get("db", "type"),
                      filename=db_path)
-        # db.bind(provider=cfg.get("db", "type"), filename=db_path, create_db=True)
-        # db.generate_mapping(create_tables=True)
         print('create db')
     else:
 
         try:
-            # db.bind(provider=cfg.get("db", "type"), filename=db_path)
-            # db.generate_mapping()
             db_l.connect(allow_auto_upgrade=True,
-                         # create_tables=True,
-                         # create_db=True,
                          provider=cfg.get("db", "type"),
                          filename=db_path)
         except Exception as e:
@@ -113,7 +107,59 @@ def is_DB_created(db_path=DB_PATH, deep=0, db_l=db):
                 print('\n=========================================\n\n\t\tдля создания новой БД перезапустите код.....')
                 print('\n=========================================')
                 exit()
-                # is_DB_created(db_path=db_path, deep=deep + 1)
+                # connect_with_db(db_path=db_path, deep=deep + 1)
+
+
+is_DB_created = connect_with_db
+
+
+def old_connect_with_db(db_path=DB_PATH, deep=0, db_l=db):
+    """
+    Создает соединение с БД для Pony ORM версии 0.73
+    :param db_path: путь к БД
+    :param deep: глубина рекурсии
+    :param db_l: объект БД
+    :return:
+    """
+    from os.path import isfile, split, join
+    from os import remove, rename
+    from sys import exit
+    from time import ctime
+    from shutil import copy as shutil_copy
+
+    if deep > 5:
+        print('в коннекте с базой данных наблюдается большая рекурсия, значит что-то идет не так')
+        exit()
+
+    if not isfile(db_path):
+        db.bind(provider=cfg.get("db", "type"), filename=db_path, create_db=True)
+        db.generate_mapping(create_tables=True)
+        print('create db')
+    else:
+
+        try:
+            db.bind(provider=cfg.get("db", "type"), filename=db_path)
+            db.generate_mapping()
+        except Exception as e:
+            print('при создании бд произошла какая-то ошибка (видимо, структура БД была изменена)\n', e)
+            print('попытка исправить.....')
+            try:
+                db.bind(provider=cfg.get("db", "type"), filename=db_path, create_tables=True,)
+                db.generate_mapping()
+                print('получилось')
+            except Exception as e:
+                print("Создаём бекап а затем удаляем БД")
+                t = ctime().split()[1:]
+                t[0], t[1], t[2] = t[2], t[1], t[0]
+                copy_name = shutil_copy(db_path, DB_BACKUPS)
+                new_name = join(split(copy_name)[0], '_'.join(t).replace(":", "-") + "_" + split(db_path)[1])
+                rename(copy_name, new_name)
+                print("создан бекап:", new_name)
+                print("Удалена исходная база данных, создаём новую")
+                remove(db_path)
+                print('\n=========================================\n\n\t\tдля создания новой БД перезапустите код.....')
+                print('\n=========================================')
+                exit()
 
 
 if __name__ == '__main__':
