e02bd89e8cb5b1c11358fcd1c5f6d66e551a358a
ch1llay <ch1llay@yandex.ru>
2021-02-19 19:19:18 +0300

    добавлено обновление на сервер через GitHub

--- app/bot/main.py
+++ app/bot/main.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 
+
+
+if __name__ == '__main__':
+    from os import chdir, getcwd
+    from os.path import split
+    print("*"*100)
+    dir_ = split(split(getcwd())[0])[0]
+    print(dir_)
+    print("*" * 100)
+    chdir(dir_)
 from app.db.all_tools_db import *
 from app.bot.base.libs import *
-
-
 if __name__ == '__main__':
     connect_with_db()
     create_test_db_1()
