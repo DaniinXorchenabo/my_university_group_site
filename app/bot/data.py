3500ff6d29f9f485a074c1c0a44eec984dd2b792
ch1llay <ch1llay@yandex.ru>
2021-02-28 16:25:51 +0300

    добавлена проверка payload

--- app/bot/data.py
+++ app/bot/data.py
@@ -1,7 +1,7 @@
-
-with open("raspisanie_par.txt", encoding="utf-8") as f:
+path = "app/bot/"
+with open(path + "raspisanie_par.txt", encoding="utf-8") as f:
     raspisanie_par = f.read()
-with open("raspisanie_first_week.txt", encoding="utf-8") as f:
+with open(path + "raspisanie_first_week.txt", encoding="utf-8") as f:
     raspisanie_first_week = f.read()
-with open("raspisanie_second_week.txt", encoding="utf-8") as f:
+with open(path + "raspisanie_second_week.txt", encoding="utf-8") as f:
     raspisanie_second_week = f.read()
