d1e34fbce632f8704514b82235f5d7df044912db
ch1llay <64579539+ch1llay@users.noreply.github.com>
2021-03-01 20:57:35 +0300

    добавлен перевод всех кнопок на callback button 1

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -93,7 +93,7 @@ keyboard.add_callback_button("ФИО преподавателей", color=VkKeyb
 keyboard.add_line()
 keyboard.add_openlink_button("Ссылка на диск", "https://yadi.sk/d/0W7wTf29wwaOYw")
 keyboard.add_line()
-keyboard.add_callback_button("Зацени", payload='{"type":"show_snackbar", text="Я полетело!"')
+keyboard.add_callback_button("Зацени", payload='{"type":"show_snackbar", text="Я полетело!"}')
 
 subjects_keyboard = VkKeyboard(one_time=False)
 subjects_keyboard.add_callback_button("Английский")
