c7ddd7dd60037f473fd0a6757b9ac2c140c7c883
ch1llay <ch1llay@yandex.ru>
2021-03-02 15:56:59 +0300

    fixing bugs

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -103,7 +103,7 @@ keyboard_my.add_callback_button("Defend", color=VkKeyboardColor.NEGATIVE, payloa
 keyboard_my.add_line()
 # keyboard.add_callback_button("ФИО преподавателей", color=VkKeyboardColor.POSITIVE, payload='{"payload":"prepody"}')
 # keyboard.add_line()
-keyboard.add_openlink_button("Ссылка на диск", "https://yadi.sk/d/0W7wTf29wwaOYw")
+keyboard_my.add_openlink_button("Ссылка на диск", "https://yadi.sk/d/0W7wTf29wwaOYw")
 
 subjects_keyboard = VkKeyboard(inline=True)
 subjects_keyboard.add_callback_button("Английский", payload={"payload": "english"})
