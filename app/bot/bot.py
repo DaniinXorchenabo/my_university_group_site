3e91672c685756d58c89556c6753e15e332f85d5
DaniinXorchenabo <rkbcu@mail.ru>
2021-03-06 10:48:33 +0300

    fixing the bag with payload callback buttons

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -163,7 +163,7 @@ def processing_msg(command: str, data: dict, send_method=vk.messages.sendMessage
             user_id=user_id,
         )
     else:
-        print(data['object'], send_method)
+        print(data['object'], send_method, vk.messages.sendMessageEventAnswer)
         message = data['object']["message"]
         from_id = message["from_id"]
         peer_id = message['peer_id']
